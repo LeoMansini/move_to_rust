6487ebac74a9ab43326a83ea94cdc79d8de6d9f3
Leo Mansini <leomansini2000@gmail.com>
2025-01-04 16:11:08 -0300

    inorder rust first version

---
+++ src/example_contracts/inorder/inorder.rs
@@ -0,0 +1,90 @@
+use crate::sui_std::transfer::transfer;
+
+pub struct CallRegistry {
+    id: u8,
+    numCalls: u64,
+    a_called: bool,
+    b_called: bool,
+    c_called: bool,
+}
+
+use std::sync::LazyLock;
+
+pub struct IdGetter {
+    current_id: std::sync::Mutex<u8>,
+}
+
+impl IdGetter {
+    pub fn new() -> Self {
+        IdGetter {
+            current_id: std::sync::Mutex::new(0),
+        }
+    }
+
+    pub fn get_new_id(&self) -> u8 {
+        let mut id = self.current_id.lock().unwrap();
+        *id += 1;
+        *id
+    }
+}
+
+// Use LazyLock to initialize ID_GETTER
+pub static ID_GETTER: LazyLock<IdGetter> = LazyLock::new(|| IdGetter::new());
+
+pub struct flashloan__flashloan {}
+impl flashloan__flashloan {
+    const EACallRequired: u64 = 0;
+    const EBCallRequired: u64 = 1;
+    const ECCallRequired: u64 = 2;
+    const EInvariantBroken: u64 = 3;
+
+    fn init() {
+        let call_registry = CallRegistry {
+            id: ID_GETTER.get_new_id(),
+            numCalls: 0,
+            a_called: false,
+            b_called: false,
+            c_called: false,
+        };
+
+        let veriman = call_registry.a_called;
+        assert!(!veriman || call_registry.a_called, "{}", EInvariantBroken);
+
+        transfer::share_object(pool);
+    }
+
+    pub fn a(call_registry: &mut CallRegistry) {
+        let veriman = call_registry.a_called;
+        call_registry.a_called = true;
+        call_registry.numCalls+=1;
+
+        assert!(!veriman || call_registry.a_called, "{}", EInvariantBroken);
+    }
+
+    pub fn b(call_registry: &mut CallRegistry) {
+        let veriman = call_registry.a_called;
+        assert!(call_registry.a_called, "{}", EACallRequired);
+        
+        if call_registry.b_called {
+            call_registry.numCalls = 0;
+            assert!(!veriman || call_registry.a_called, "{}", EInvariantBroken);
+            return;
+        }
+
+        call_registry.b_called = true;
+        call_registry.numCalls+=1;
+
+        assert!(!veriman || call_registry.a_called, "{}", EInvariantBroken);
+    }
+
+    pub fn c(call_registry: &mut CallRegistry) {
+        assert!(call_registry.a_called, "{}", EACallRequired);
+        assert!(call_registry.b_called, "{}", EBCallRequired);
+        
+        call_registry.c_called = true;
+        call_registry.numCalls+=1;
+
+        return 3;
+    }
+
+}   
