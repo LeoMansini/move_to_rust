bb5d19305c8fda4648749f1a821ebd53291aa442
Leo Mansini <leomansini2000@gmail.com>
2025-02-22 18:46:28 -0300

    running harness, doens't end...

--- src/harnesses/dschief_harnesses.rs
+++ src/harnesses/dschief_harnesses.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 
 impl Arbitrary for Address {
     // Custom method to generate arbitrary `Address`
-    fn arbitrary() -> Self {
+    fn any() -> Self {
         // Generate arbitrary `u64` value for `id`
         Address {
             id: kani::any(),
@@ -13,41 +13,41 @@ impl Arbitrary for Address {
     }
 }
 
-impl<K, V> Arbitrary for HashMap<K, V>
+pub fn arbitrary_hashmap<K, V>() -> HashMap<K, V>
 where
-    K: Arbitrary + Eq + std::hash::Hash,
-    V: Arbitrary,
+    K: Arbitrary + Eq + std::hash::Hash + Clone,
+    V: Arbitrary + Clone,
 {
-    fn arbitrary() -> Self {
-        let mut map = HashMap::new();
-        let size = kani::any::<u32>() % 10; // Limit the number of entries to 10
+    let mut map = HashMap::new();
+    let size: u8 = kani::any();
+    kani::assume(size < 10); // Limit the map size
 
-        for _ in 0..size {
-            let key = K::arbitrary(); // Generate arbitrary key
-            let value = V::arbitrary(); // Generate arbitrary value
-            map.insert(key, value);
-        }
-
-        map
+    for _ in 0..size {
+        let key: K = kani::any();
+        let value: V = kani::any();
+        map.insert(key, value);
     }
+
+    map
 }
 
-impl<K: Key, V> Arbitrary for Table<K, V> {
+impl<K: Key + Arbitrary + Clone, V: Arbitrary + Clone> Arbitrary for Table<K, V> {
     // Custom method to generate arbitrary `Address`
-    fn arbitrary() -> Self {
+    fn any() -> Self {
         // Generate arbitrary `u64` value for `id`
-        let map: HashMap<K, V> = kani::any();
+        let map = arbitrary_hashmap();
+        let size = map.len();
         Table {
             id: kani::any(),
             map: map,
-            size: map.len(),
+            size: size as u8, // Cast is safe as size is less than 10
         }
     }
 }
 
 impl Arbitrary for DSChief {
     // Custom method to generate arbitrary `Address`
-    fn arbitrary() -> Self {
+    fn any() -> Self {
         // Generate arbitrary `u64` value for `id`
         DSChief {
             id: kani::any(),
@@ -61,16 +61,16 @@ impl Arbitrary for DSChief {
 
 
 #[kani::proof]
-#[kani::unwind(5)]
+#[kani::unwind(3)]
 pub fn try_generic_dschief() {
     let mut dschief: DSChief = kani::any();
     while true {
         let x: u8 = kani::any();
         kani::assume(x < 3);
         match x {
-            0=> SimpleDSChief__SimpleDSChief::lock(&mut dschief, kani::any(), kani::any()),
-            1=> SimpleDSChief__SimpleDSChief::free(&mut dschief, kani::any(), kani::any()),
-            2=> {SimpleDSChief__SimpleDSChief::voteYays(&mut dschief, kani::any(), kani::any());},
+            0=> SimpleDSChief__SimpleDSChief::lock(&mut dschief, &kani::any(), kani::any()),
+            1=> SimpleDSChief__SimpleDSChief::free(&mut dschief, &kani::any(), kani::any()),
+            2=> {SimpleDSChief__SimpleDSChief::voteYays(&mut dschief, &kani::any(), kani::any());},
             _=>{}
         }
     }
